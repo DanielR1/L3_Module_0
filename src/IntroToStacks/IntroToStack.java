c8b5551ad5261c45959a9f0bb3d7e3554b5aaa58
DanielR1 <league@WorthyMini-5.attlocal.net>
2017-08-17 19:56:19 -0700

    r
    r

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -1,13 +1,56 @@
 package IntroToStacks;
 
+import java.util.Random;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
+		Stack<Double> numbers = new Stack<Double>();
 		
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		for (int i = 0; i < 100; i++) {
+			numbers.push((new Random().nextDouble())*100);
+		
+			
+		}
 		
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		JOptionPane.showMessageDialog(null, "Enter 2 numbers between 0 and 100, smallest number first");
+		String a = JOptionPane.showInputDialog("First number:");
+		int c = Integer.parseInt(a);
+		if (c>100||c<0 ){
+			JOptionPane.showMessageDialog(null, "That is not a number between 0 and 100!");
+			System.exit(0);
+		}
+		String b = JOptionPane.showInputDialog("Second Number:");
+		
+		int d = Integer.parseInt(b);
+		if (d>100||d<0 ){
+			JOptionPane.showMessageDialog(null, "That is not a number between 0 and 100!");
+			System.exit(0);
+		}
+		if(c>=d) {
+			JOptionPane.showMessageDialog(null, "Second number is not larger than first number!");
+			System.exit(0);
+		}
+		JOptionPane.showMessageDialog(null, "Popping elements off stack...");
+		String bigstring="Elements between "+c+" and "+d+":";
+		for (int i = 0; i < 100; i++) {
+			double v=numbers.pop();
+			if(v>c) {
+				if(v<d) {
+					bigstring+="\n"+v;
+				}
+			}
+		}
+		JOptionPane.showMessageDialog(null, bigstring);
+		
+		
+		
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,18 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
+	public static void main(String[] args) {
+		TextUndoRedo text = new TextUndoRedo();
+		text.setup();
+	}
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +25,34 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	Stack<String>stack =new Stack<String>();
+	void setup() {
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+		
+		frame.setVisible(true);
+		frame.pack();
+		frame.setSize(500, 500);
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getKeyChar());
+		label.setText(label.getText()+e.getKeyChar());
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
